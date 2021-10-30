6b9b4a81c6d4f4f5d69536ea24bb12a444c91e26
Prez Ivan <ivpcode@gmail.com>
2021-10-30 18:04:37 +0200

    sisteamto problema viste

--- sqres.js
+++ sqres.js
@@ -23,19 +23,16 @@ let work_dir = process.argv[2];
                 await image.decoded; //Wait until the image is decoded before running preprocessors. 
 
                 let dims = [ 640, 1024, 1280, 1920]
-                let subhtml = ""
+                
 
-                for(let i = 0; i<dims.length; i++) {
+                for(let i = dims.length-1; i>=0; i--) {
                     let dim = dims[i]
                     try {
-                        const preprocessOptions = {
+                        await image.preprocess({
                             resize: {"enabled":true,"width":dim,"method":"lanczos3","fitMethod":"stretch","premultiply":true,"linearRGB":true},
-
-                        }
-                        await image.preprocess(preprocessOptions);
+                        });
 
                         const encodeOptions = {
-                            resize: {"enabled":true,"width":dim,"method":"lanczos3","fitMethod":"stretch","premultiply":true,"linearRGB":true},
                             mozjpeg: {"quality":90,"baseline":false,"arithmetic":false,"progressive":true,"optimize_coding":true,"smoothing":0,"color_space":3,"quant_table":3,"trellis_multipass":false,"trellis_opt_zero":false,"trellis_opt_table":false,"trellis_loops":1,"auto_subsample":true,"chroma_subsample":2,"separate_chroma_quality":false,"chroma_quality":75}  
                         }
                         await image.encode(encodeOptions);
@@ -47,17 +44,23 @@ let work_dir = process.argv[2];
 
                         let flname = `/${dim}/` + path.basename(imagePath.replace(".png",".jpg"))
                         fs.writeFileSync(work_dir + flname, rawEncodedImage)
-                        
-                        if (subhtml == "")
-                            subhtml = `<img loading="lazy" src="${path.basename(work_dir)+flname}"\n\tsrcset="${path.basename(work_dir)+flname} ${dim}w`
-                        else
-                            subhtml += ` ${path.basename(work_dir)+flname} ${dim}w`
-                        if (i<dims.length-1)
-                            subhtml+=","
 
                     } catch (Ex) {
                         console.log("Inner error: "+Ex)
-                    }
+                    }                    
+                }
+
+                let subhtml = ""
+                for(let i = 0; i<dims.length; i++) {
+                    let dim = dims[i]
+                    let flname = `/${dim}/` + path.basename(imagePath.replace(".png",".jpg"))
+                    
+                    if (subhtml == "")
+                        subhtml = `<img loading="lazy" src="${path.basename(work_dir)+flname}"\n\tsrcset="${path.basename(work_dir)+flname} ${dim}w`
+                    else 
+                        subhtml += ` ${path.basename(work_dir)+flname} ${dim}w`
+                    if (i<dims.length-1)
+                        subhtml+=","
                 }
                 subhtml += `" />\n`
                 html += subhtml
